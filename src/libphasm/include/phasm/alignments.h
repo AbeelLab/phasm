c2d821618cf8fafe50293262d126d55d4b895ae1
Lucas van Dijk <info@return1.net>
2017-02-16 22:09:30 +0100

    Extend the LocalAlignment class
    Add several helper functions to retrieve information about
    the alignment.

--- src/libphasm/include/phasm/alignments.h
+++ src/libphasm/include/phasm/alignments.h
@@ -1,17 +1,40 @@
 #include <string>
 #include <tuple>
+#include <algorithm>
 #include <seqan/sequence.h>
 
 #ifndef PHASM_ALIGNMENTS_H
 #define PHASM_ALIGNMENTS_H
 
-namespace phasm {
+namespace phasm
+{
+
+using std::get;
+using seqan::length;
 
 typedef std::vector<std::tuple<int, int> > TracePoints;
 
-enum class Strand : uint8_t {
-    SAME = 0,
-    OPPOSITE = 1
+/**
+ * Distinguish if two reads come from the same or opposite strand.
+ */
+enum class Strand : uint8_t
+{
+    SAME,
+    OPPOSITE
+};
+
+
+/**
+ * Distinguish between different kind of overlaps.
+ *
+ * An overlap is contained if one read completely spans the other.
+ */
+enum class AlignmentType : uint8_t
+{
+    A_CONTAINED,
+    B_CONTAINED,
+    OVERLAP_AB,
+    OVERLAP_BA
 };
 
 /**
@@ -21,33 +44,36 @@ enum class Strand : uint8_t {
  * @tparam T String type of a read, for example `seqan::DnaString`
  */
 template<typename T>
-class Read {
+class Read
+{
 public:
-    Read(const std::string& id, unsigned long length);
-    Read(const std::string& id, const T& sequence);
+    Read(const std::string &id, unsigned long length);
+    Read(const std::string &id, const T &sequence);
 
-    unsigned long getLength() const;
+    unsigned long length() const;
 
 private:
     std::string id;
     T sequence;
-    unsigned long length;
+    unsigned long length_;
 };
 
 template<typename T>
-Read<T>::Read(const std::string& id, unsigned long length) : id(id), length(length) {
+Read<T>::Read(const std::string &id, unsigned long length) : id(id), length_(length)
+{
 }
 
 template<typename T>
-Read<T>::Read(const std::string& id, const T& sequence) : id(id), sequence(sequence) {
-    this->length = seqan::length(this->sequence);
+Read<T>::Read(const std::string &id, const T &sequence) : id(id), sequence(sequence)
+{
+    length_ = seqan::length(sequence);
 }
 
 template<typename T>
-unsigned long Read<T>::getLength() const {
-    return this->length;
+unsigned long Read<T>::length() const
+{
+    return length_;
 }
-
 
 
 /**
@@ -58,36 +84,112 @@ unsigned long Read<T>::getLength() const {
  * @tparam T
  */
 template<typename T>
-class LocalAlignment {
+class LocalAlignment
+{
 public:
-    LocalAlignment(const Read<T>& a, const Read<T>& b, Strand strand, const std::tuple<int, int>& arange,
-                   const std::tuple<int, int>& brange);
-    LocalAlignment(const Read<T>& a, const Read<T>& b, Strand strand, const std::tuple<int, int>& arange,
-                   const std::tuple<int, int>& brange, const TracePoints& trace_points);
+    LocalAlignment(const Read<T> &a, const Read<T> &b, Strand strand, const std::tuple<int, int> &arange,
+                   const std::tuple<int, int> &brange, int differences);
+
+    LocalAlignment(const Read<T> &a, const Read<T> &b, Strand strand, const std::tuple<int, int> &arange,
+                   const std::tuple<int, int> &brange, int differences, const TracePoints &trace_points);
+
+    const Read<T> &readA() const;
+    const Read<T> &readB() const;
+
+    const std::tuple<int, int> &getArange() const;
+    const std::tuple<int, int> &getBrange() const;
+
+    int getOverlapLength() const;
+    int getDifferences() const;
+    float getErrorRate() const;
+    unsigned long getOverhang() const;
+
+    AlignmentType classify() const;
 
 private:
-    const Read<T>& a;
-    const Read<T>& b;
+    const Read<T> &a;
+    const Read<T> &b;
     Strand strand;
     std::tuple<int, int> arange;
     std::tuple<int, int> brange;
+    int differences;
     TracePoints trace_points;
 
 };
 
 template<typename T>
-LocalAlignment<T>::LocalAlignment(const Read<T> &a, const Read<T> &b, Strand strand, const std::tuple<int, int>& arange,
-                                  const std::tuple<int, int>& brange
-)  : a(a), b(b), strand(strand), arange(arange), brange(brange)
+LocalAlignment<T>::LocalAlignment(const Read<T> &a, const Read<T> &b, Strand strand, const std::tuple<int, int> &arange,
+                                  const std::tuple<int, int> &brange, int differences
+)  : a(a), b(b), strand(strand), arange(arange), brange(brange), differences(differences)
 {
 }
 
 template<typename T>
-LocalAlignment<T>::LocalAlignment(const Read<T> &a, const Read<T> &b, Strand strand, const std::tuple<int, int>& arange,
-                                  const std::tuple<int, int>& brange, const TracePoints& trace_points
-)  : a(a), b(b), strand(strand), arange(arange), brange(brange), trace_points(trace_points)
+LocalAlignment<T>::LocalAlignment(const Read<T> &a, const Read<T> &b, Strand strand, const std::tuple<int, int> &arange,
+                                  const std::tuple<int, int> &brange, int differences, const TracePoints &trace_points
+)  : a(a), b(b), strand(strand), arange(arange), brange(brange), differences(differences), trace_points(trace_points)
 {
 }
+
+template<typename T>
+const Read<T> &LocalAlignment<T>::readA() const
+{
+    return a;
+}
+
+template<typename T>
+const Read<T> &LocalAlignment<T>::readB() const
+{
+    return b;
+}
+
+template<typename T>
+const std::tuple<int, int> &LocalAlignment<T>::getArange() const
+{
+    return arange;
+}
+
+template<typename T>
+const std::tuple<int, int> &LocalAlignment<T>::getBrange() const
+{
+    return brange;
+}
+
+template<typename T>
+int LocalAlignment<T>::getOverlapLength() const
+{
+    return std::min(get<1>(arange) - get<0>(arange), get<1>(brange) - get<0>(brange));
+}
+
+template<typename T>
+unsigned long LocalAlignment<T>::getOverhang() const
+{
+    return (std::min(get<0>(arange), get<0>(brange)) +
+            std::min(a.length() - get<1>(arange), b.length() - get<1>(brange)));
+}
+
+template<typename T>
+AlignmentType LocalAlignment<T>::classify() const
+{
+    if (get<0>(arange) <= get<0>(brange) && (a.length() - get<1>(arange)) <= (b.length() - get<1>(brange)))
+    {
+        return AlignmentType::A_CONTAINED;
+    }
+    else if (get<0>(arange) >= get<0>(brange) &&
+             (a.length() - get<1>(arange)) >= (b.length() - get<1>(brange)))
+    {
+        return AlignmentType::B_CONTAINED;
+    }
+    else if (get<0>(arange) > get<0>(brange))
+    {
+        return AlignmentType::OVERLAP_AB;
+    }
+    else
+    {
+        return AlignmentType::OVERLAP_BA;
+    }
+}
+
 
 } // namespace phasm
 
--- src/libphasm/tests/reads.cpp
+++ src/libphasm/tests/reads.cpp
@@ -6,6 +6,6 @@
 using seqan::DnaString;
 
 TEST_CASE("Reads have proper length", "[reads]") {
-    REQUIRE(phasm::Read<DnaString>("id", 10).getLength() == 10);
-    REQUIRE(phasm::Read<DnaString>("id", "ACTGATCGCA").getLength() == 10);
+    REQUIRE(phasm::Read<DnaString>("id", 10).length() == 10);
+    REQUIRE(phasm::Read<DnaString>("id", "ACTGATCGCA").length() == 10);
 }
