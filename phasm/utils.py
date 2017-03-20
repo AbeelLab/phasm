9b70c84870f4c12e82b7ea4d84a3778ff2206abb
Lucas van Dijk <info@return1.net>
2017-03-20 15:24:56 +0100

    Add utilities module

---
+++ phasm/utils.py
@@ -0,0 +1,18 @@
+def round_up(number: int, multiple: int) -> int:
+    """Round a number up to a multiple of another number. Only works for
+    positive numbers.
+
+    Example:
+
+    >>> round_up(57, 100)
+    100
+    >>> round_up(102, 100)
+    200
+    >>> round_up(43, 50)
+    50
+    >>> round_up(77, 50)
+    100
+    """
+    assert multiple != 0
+
+    return int((number + multiple - 1) / multiple) * multiple
