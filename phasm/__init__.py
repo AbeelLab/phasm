0fe127d567d34fa3be4330bd2557a630850ee9bd
Lucas van Dijk <info@return1.net>
2017-03-20 15:28:40 +0100

    Update setup.py to include pytest

---
+++ phasm/__init__.py
@@ -0,0 +1,5 @@
+"""PHASM: a haplotype-aware de novo genome assembler which tries to output
+contigs for each chromosome in an organism. The main focus of this assembler is
+constructing genomes from aneuploid organisms."""
+
+__version__ = '0.1'
