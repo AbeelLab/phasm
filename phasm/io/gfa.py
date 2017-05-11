0bcb6c1bcffd63741a09a1ebd1f831c44ffa426b
Lucas van Dijk <info@return1.net>
2017-05-11 14:21:21 +0200

    Add GFA2 parsing code

--- phasm/io/gfa.py
+++ phasm/io/gfa.py
@@ -5,10 +5,12 @@ Utilities to output Graphical Assembly Format-files
 Tools to convert a NetworkX assembly graph to a GFA file.
 """
 
-from typing import TextIO
+from typing import TextIO, Mapping
 
+import numpy
 import gfapy
 
+from phasm.alignments import Read, LocalAlignment, Strand
 from phasm.assembly_graph import AssemblyGraph
 
 
@@ -17,7 +19,41 @@ def parse_gfa_line(line: str):
 
 
 def gfa_line_to_read(line: gfapy.Line):
-    pass
+    if not str(line).startswith('S'):
+        raise ValueError("Given GFA2 line is not a segment.")
+
+    sequence = None
+    if not gfapy.is_placeholder(line.sequence):
+        sequence = numpy.array(line.sequence.encode('ascii'), dtype='S')
+
+    return Read(line.sid, line.slen, sequence)
+
+
+def gfa_line_to_la(reads: Mapping[str, Read]):
+    def mapper(line: gfapy.Line):
+        if not str(line).startswith('E'):
+            raise ValueError('Given GFA2 line is not an edge.')
+
+        a_read = reads[line.sid1.name]
+        b_read = reads[line.sid2.name]
+
+        if line.sid1.orient == '-':
+            raise ValueError("A-read as reverse complement, unsupported by "
+                             "phasm.")
+
+        strand = Strand.SAME if line.sid2.orient == '+' else Strand.OPPOSITE
+
+        alignment = None
+        if isinstance(line.alignment, gfapy.CIGAR):
+            alignment = str(line.alignment)
+        elif isinstance(line.alignment, gfapy.Trace):
+            alignment = list(line.alignment)
+
+        return LocalAlignment(a_read, b_read, strand,
+                              (int(line.beg1), int(line.end1)),
+                              (int(line.beg2), int(line.end2)), alignment)
+
+    return mapper
 
 
 def gfa_line(*args) -> str:
@@ -41,7 +77,7 @@ def write_graph(f: TextIO, g: AssemblyGraph, gfa_version: int=1):
 
 
 def _write_graph_gfa1(f: TextIO, g: AssemblyGraph):
-    lines = ["H\tVN:Z:1.0\n"]
+    f.write(gfa_header("1.0"))
 
     segments = set()
     for n in g.nodes_iter():
@@ -51,8 +87,8 @@ def _write_graph_gfa1(f: TextIO, g: AssemblyGraph):
 
         segments.add(segment_name)
 
-        line = "S\t{}\t*\tLN:i:{}\n".format(segment_name, n.length)
-        lines.append(line)
+        parts = ["S", segment_name, "*", "LN:i:{}".format(n.length)]
+        f.write(gfa_line(*parts))
 
     for u, v, d in g.edges_iter(data=True):
         uid = str(u)[:-1]
@@ -62,11 +98,9 @@ def _write_graph_gfa1(f: TextIO, g: AssemblyGraph):
 
         # Fake CIGAR string just indicating overlap length
         overlap = str(d['overlap_len'])+"M"
-        line = "L\t{}\t{}\t{}\t{}\t{}\n".format(
-            uid, u_strand, vid, v_strand, overlap)
-        lines.append(line)
 
-    f.writelines(lines)
+        parts = ["L", uid, u_strand, vid, v_strand, overlap]
+        f.write(gfa_line(*parts))
 
 
 def _write_graph_gfa2(f: TextIO, g: AssemblyGraph):
