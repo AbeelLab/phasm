ede804cf9ea6b13a9b6a8913acfad5fab1cea87e
Lucas van Dijk <info@return1.net>
2017-05-11 14:23:34 +0200

    Read LocalAlignments from a GFA2 file
    We're not dependent on DALIGNER anymore.

--- test.py
+++ test.py
@@ -1,17 +1,15 @@
-import sys
 import logging
 import argparse
-import subprocess
+from collections import defaultdict
 
 import networkx
 
-from phasm.io import daligner, gfa
+from phasm.io import gfa
 from phasm.assembly_graph import (build_assembly_graph,
                                   remove_transitive_edges, clean_graph,
-                                  remove_tips, remove_short_overlaps,
-                                  make_symmetric, merge_unambiguous_paths)
-from phasm.filter import (ContainedReads, MaxOverhang, MinReadLength,
-                          MinOverlapLength)
+                                  remove_tips, make_symmetric,
+                                  merge_unambiguous_paths)
+from phasm.filter import ContainedReads, MaxOverhang, MinReadLength
 from phasm.walker import build_haplographs
 
 logging.basicConfig()
@@ -22,57 +20,68 @@ logger.setLevel(logging.INFO)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Build string graph from pairwise overlaps")
-    parser.add_argument('database',
-                        help="DAZZ_DB main database file")
     parser.add_argument(
-        'alignments', type=argparse.FileType(), nargs='?', default=sys.stdin,
-        help="File to read local alignments in LAdump format from"
+        'gfa_file',
+        help="GFA2 file with all reads (segments) and pairwise alignments."
     )
 
     args = parser.parse_args()
     logger.info("======== STAGE 1: Build Assembly Graph =========")
 
+    logger.info("Pass [1/2] of GFA2 file to import reads (segments)...")
     reads = {}
-    with subprocess.Popen(["DBdump", "-rh", args.database],
-                          stdout=subprocess.PIPE,
-                          universal_newlines=True) as proc:
-        reads = {r.id: r for r in daligner.parse_reads(proc.stdout)}
+    with open(args.gfa_file) as f:
+        read_iter = map(gfa.parse_gfa_line,
+                        (l.strip() for l in f if l.startswith('S')))
+        read_iter = map(gfa.gfa_line_to_read, read_iter)
+        reads = {r.id: r for r in read_iter}
 
-        logger.info("Read %d reads from DAZZ_DB database.", len(reads))
+        logger.info("Read %d reads from GFA2 file.", len(reads))
 
-    la_iter = iter(daligner.parse_local_alignments(reads, args.alignments))
+    logger.info("Pass [2/2] of GFA2 file to import local alignments "
+                "(edges)...")
+    # logger.setLevel(logging.DEBUG)
+    with open(args.gfa_file) as f:
+        la_iter = map(gfa.parse_gfa_line,
+                      (l.strip() for l in f if l.startswith('E')))
+        la_iter = map(gfa.gfa_line_to_la(reads), la_iter)
 
-    # Apply read/alignment filters
-    filters = [
-        MinReadLength(5000),
-        MinOverlapLength(2000),
-        ContainedReads(),
-        MaxOverhang(1000, 0.8)
-    ]
+        # Apply read/alignment filters
+        filters = [
+            MinReadLength(5000),
+            ContainedReads(),
+            MaxOverhang(1000, 0.8)
+        ]
 
-    la_iter = filter(lambda x: all(f(x) for f in filters), la_iter)
+        la_iter = filter(lambda x: all(f(x) for f in filters), la_iter)
 
-    g = build_assembly_graph(la_iter)
+        g = build_assembly_graph(la_iter)
+        logger.setLevel(logging.INFO)
 
-    for f in filters:
-        logger.info("Filter %s removed %d alignments.", f.__class__.__name__,
-                    f.filtered)
+        logger.info("Built initial assembly graph with %d nodes and %d "
+                    "edges.",
+                    networkx.number_of_nodes(g),
+                    networkx.number_of_edges(g))
 
-    logger.info("Contained read removal stage 2... (%d contained reads)",
-                len(filters[2].contained_reads))
+        for f in filters:
+            logger.info("Filter %s removed %d alignments.",
+                        f.__class__.__name__, f.filtered)
 
-    for read in filters[2].contained_reads:
-        orig = read.with_orientation('-')
-        reverse = read.with_orientation('+')
+        logger.info("Contained read removal stage 2... (%d contained reads)",
+                    len(filters[1].contained_reads))
 
-        if orig in g:
-            g.remove_node(orig)
+        for read in filters[1].contained_reads:
+            orig = read.with_orientation('-')
+            reverse = read.with_orientation('+')
 
-        if reverse in g:
-            g.remove_node(reverse)
+            if orig in g:
+                g.remove_node(orig)
 
-    # Free up memory
-    del filters
+            if reverse in g:
+                g.remove_node(reverse)
+
+        # Free up memory
+        del filters
 
     logger.info("After contained removal: graph with %d nodes and %d edges.",
                 networkx.number_of_nodes(g),
@@ -80,6 +89,8 @@ if __name__ == '__main__':
 
     with open("assembly_graph.gfa", "w") as f:
         gfa.write_graph(f, g)
+
+    networkx.write_graphml(g, "assembly_graph.graphml")
 
     logger.info("======== STAGE 2: Graph Cleaning =========")
 
@@ -93,30 +104,12 @@ if __name__ == '__main__':
     num_in_tips, num_out_tips = remove_tips(g)
     num_asymm_edges += make_symmetric(g)
 
-    logger.info("Removing short overlaps...")
-
-    num_short_overlaps = 0
-    # for ratio in [0.5, 0.6, 0.7]:
-    #     short_overlap_edges = list(remove_short_overlaps(g, ratio))
-    #     g.remove_edges_from(short_overlap_edges)
-    #     num_short_overlaps += len(short_overlap_edges)
-
-    #     if num_short_overlaps != 0:
-    #         num_asymm_edges += make_symmetric(g)
-
-    #         in_tips, out_tips = remove_tips(g)
-    #         num_in_tips += in_tips
-    #         num_out_tips += out_tips
-
-    num_asymm_edges += make_symmetric(g)
-
     logger.info("Removing isolated nodes...")
     num_isolated_nodes = clean_graph(g)
 
-    logger.info("Removed %d tip edges, %d short overlaps, %d isolated nodes, "
-                "%d asymmetric edges.",
-                num_in_tips+num_out_tips, num_short_overlaps,
-                num_isolated_nodes, num_asymm_edges)
+    logger.info("Removed %d tip edges, %d isolated nodes, %d asymmetric "
+                "edges.",
+                num_in_tips+num_out_tips, num_isolated_nodes, num_asymm_edges)
 
     logger.info("Removing tips (stage 2)...")
     num_in_tips, num_out_tips = remove_tips(g)
@@ -145,21 +138,43 @@ if __name__ == '__main__':
 
     networkx.write_graphml(g, "assembly_graph_reduced.graphml")
 
-    logger.info("======== STAGE 3: Extract Haplotype Graphs =========")
+    logger.info("======== STAGE 3: Extract and Phase Haplotypes =========")
 
-    logger.setLevel(logging.DEBUG)
+    # Start with reading all pairwise mappings, so we can easily retreive which
+    # reads we need for likelihood calculation
+    logger.info("Reading all pairwise mappings...")
+    pairwise_mappings = defaultdict(list)
+    with open(args.gfa_file) as f:
+        la_iter = map(gfa.parse_gfa_line,
+                      (l.strip() for l in f if l.startswith('E')))
+        la_iter = map(gfa.gfa_line_to_la(reads), la_iter)
 
+        for la in la_iter:
+            pairwise_mappings[la.a.id].append(la)
+
+    logger.info("Done.")
+    logger.info("Building haplographs...")
+
+    num_components = 0
+    num_haplographs = 0
     for i, component in enumerate(
             networkx.weakly_connected_component_subgraphs(g, copy=False)):
-        logger.info("Connected Component %d with %d nodes and %d edges",
-                    i, networkx.number_of_nodes(component),
-                    networkx.number_of_edges(component))
+        logger.debug("Connected Component %d with %d nodes and %d edges",
+                     i, networkx.number_of_nodes(component),
+                     networkx.number_of_edges(component))
 
         for j, haplograph in enumerate(build_haplographs(component)):
-            logger.info("- Haplograph %d with %d nodes and %d edges",
-                        j, networkx.number_of_nodes(haplograph),
-                        networkx.number_of_edges(haplograph))
+            logger.debug("- Haplograph %d with %d nodes and %d edges",
+                         j, networkx.number_of_nodes(haplograph),
+                         networkx.number_of_edges(haplograph))
 
             filename = "component{}.haplograph{}.gfa".format(i, j)
             with open(filename, "w") as f:
                 gfa.write_graph(f, haplograph)
+
+            num_haplographs += 1
+
+        num_components += 1
+
+    logger.info("Built %d haplographs from %d weakly connected components.",
+                num_haplographs, num_components)
