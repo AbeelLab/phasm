609203198caa94f6db218e7cabd55ad034d1111b
Lucas van Dijk <info@return1.net>
2017-05-28 18:37:29 +0200

    Add test code to try the phasing algorithm

--- test.py
+++ test.py
@@ -3,18 +3,29 @@ import argparse
 from collections import defaultdict
 
 import networkx
+import dinopy
 
-from phasm.io import gfa
+from phasm.io import gfa, sequences
 from phasm.assembly_graph import (build_assembly_graph,
                                   remove_transitive_edges, clean_graph,
                                   remove_tips, make_symmetric,
                                   merge_unambiguous_paths)
 from phasm.filter import ContainedReads, MaxOverhang, MinReadLength
 from phasm.walker import build_haplographs
+from phasm.phasing import HaploGraphPhaser, MismatchErrorModel
 
 logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+
+def phase_haplograph(hg, alignments, error_model):
+    phaser = HaploGraphPhaser(hg, alignments, 3, error_model, 0.1, 0.8)
+
+    haplotypes = phaser.phase()
+    logger.info("Got %d equally likely possible haplotypes", len(haplotypes))
+
+    return haplotypes[0]
 
 
 if __name__ == '__main__':
@@ -24,6 +35,10 @@ if __name__ == '__main__':
         'gfa_file',
         help="GFA2 file with all reads (segments) and pairwise alignments."
     )
+    parser.add_argument(
+        'sequences_file',
+        help="Original FASTA/FASTQ file with the sequence data for each read."
+    )
 
     args = parser.parse_args()
     logger.info("======== STAGE 1: Build Assembly Graph =========")
@@ -31,20 +46,17 @@ if __name__ == '__main__':
     logger.info("Pass [1/2] of GFA2 file to import reads (segments)...")
     reads = {}
     with open(args.gfa_file) as f:
-        read_iter = map(gfa.parse_gfa_line,
-                        (l.strip() for l in f if l.startswith('S')))
-        read_iter = map(gfa.gfa_line_to_read, read_iter)
+        read_iter = map(gfa.gfa_line_to_read,
+                        (l for l in f if l.startswith('S')))
         reads = {r.id: r for r in read_iter}
 
         logger.info("Read %d reads from GFA2 file.", len(reads))
 
     logger.info("Pass [2/2] of GFA2 file to import local alignments "
                 "(edges)...")
-    # logger.setLevel(logging.DEBUG)
     with open(args.gfa_file) as f:
-        la_iter = map(gfa.parse_gfa_line,
-                      (l.strip() for l in f if l.startswith('E')))
-        la_iter = map(gfa.gfa_line_to_la(reads), la_iter)
+        la_iter = map(gfa.gfa_line_to_la(reads),
+                      (l for l in f if l.startswith('E')))
 
         # Apply read/alignment filters
         filters = [
@@ -64,26 +76,28 @@ if __name__ == '__main__':
                     networkx.number_of_edges(g))
 
         for f in filters:
+            filtered = f.filtered
+
+            if f.nodes_to_remove:
+                for read in f.nodes_to_remove:
+                    orig = read.with_orientation('-')
+                    reverse = read.with_orientation('+')
+
+                    if orig in g:
+                        filtered += g.degree(orig)
+                        g.remove_node(orig)
+
+                    if reverse in g:
+                        filtered += g.degree(reverse)
+                        g.remove_node(reverse)
+
             logger.info("Filter %s removed %d alignments.",
                         f.__class__.__name__, f.filtered)
-
-        logger.info("Contained read removal stage 2... (%d contained reads)",
-                    len(filters[1].contained_reads))
-
-        for read in filters[1].contained_reads:
-            orig = read.with_orientation('-')
-            reverse = read.with_orientation('+')
-
-            if orig in g:
-                g.remove_node(orig)
-
-            if reverse in g:
-                g.remove_node(reverse)
 
         # Free up memory
         del filters
 
-    logger.info("After contained removal: graph with %d nodes and %d edges.",
+    logger.info("Final graph: %d nodes and %d edges.",
                 networkx.number_of_nodes(g),
                 networkx.number_of_edges(g))
 
@@ -145,36 +159,60 @@ if __name__ == '__main__':
     logger.info("Reading all pairwise mappings...")
     pairwise_mappings = defaultdict(list)
     with open(args.gfa_file) as f:
-        la_iter = map(gfa.parse_gfa_line,
-                      (l.strip() for l in f if l.startswith('E')))
-        la_iter = map(gfa.gfa_line_to_la(reads), la_iter)
+        la_iter = map(gfa.gfa_line_to_la(reads),
+                      (l for l in f if l.startswith('E')))
 
         for la in la_iter:
-            pairwise_mappings[la.a.id].append(la)
+            a_read, b_read = la.get_oriented_reads()
+            pairwise_mappings[a_read].append(la)
 
     logger.info("Done.")
-    logger.info("Building haplographs...")
 
+    sequence_source = sequences.FastaSource(args.sequences_file)
+    g.sequence_src = sequence_source
+
+    error_model = MismatchErrorModel(0.01)
+
+    logger.info("Building haplographs...")
     num_components = 0
     num_haplographs = 0
-    for i, component in enumerate(
-            networkx.weakly_connected_component_subgraphs(g, copy=False)):
-        logger.debug("Connected Component %d with %d nodes and %d edges",
-                     i, networkx.number_of_nodes(component),
-                     networkx.number_of_edges(component))
 
-        for j, haplograph in enumerate(build_haplographs(component)):
-            logger.debug("- Haplograph %d with %d nodes and %d edges",
-                         j, networkx.number_of_nodes(haplograph),
-                         networkx.number_of_edges(haplograph))
+    logger.setLevel(logging.DEBUG)
+    with dinopy.FastaWriter("output.fasta", force_overwrite=True) as fw:
+        for i, component in enumerate(
+                networkx.weakly_connected_component_subgraphs(g, copy=False)):
+            logger.debug("Connected Component %d with %d nodes and %d edges",
+                         i, networkx.number_of_nodes(component),
+                         networkx.number_of_edges(component))
 
-            filename = "component{}.haplograph{}.gfa".format(i, j)
-            with open(filename, "w") as f:
-                gfa.write_graph(f, haplograph)
+            for j, haplograph in enumerate(build_haplographs(component)):
+                logger.debug("- Haplograph %d with %d nodes and %d edges",
+                             j, networkx.number_of_nodes(haplograph),
+                             networkx.number_of_edges(haplograph))
 
-            num_haplographs += 1
+                filename = "component{}.haplograph{}.gfa".format(i, j)
+                with open(filename, "w") as f:
+                    gfa.write_graph(f, haplograph)
 
-        num_components += 1
+                filename = "component{}.haplograph{}.graphml".format(i, j)
+                networkx.write_graphml(haplograph, filename)
+
+                if networkx.number_of_nodes(haplograph) == 1:
+                    seq = g.get_sequence(haplograph.nodes()[0])
+                    fw.write_entry(
+                        (seq, b"haplotig%d.%d" % (num_haplographs, 0)))
+                else:
+                    haplotypes = phase_haplograph(haplograph,
+                                                  pairwise_mappings,
+                                                  error_model)
+                    for h, haplotype in enumerate(haplotypes):
+                        seq = g.sequence_for_path(haplotype)
+                        fw.write_entry(
+                            (seq, b"haplotig%d.%d" % (num_haplographs, h)))
+
+                num_haplographs += 1
+
+            num_components += 1
 
     logger.info("Built %d haplographs from %d weakly connected components.",
                 num_haplographs, num_components)
